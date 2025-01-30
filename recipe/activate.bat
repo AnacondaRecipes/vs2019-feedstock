6f9f67e4f73adffc4604d1f4b5f49efa808f2c10
Eric Lundby <Eric.Lundby@gmail.com>
2025-01-30 14:12:11 -0700

    set INCLUDE, LIB and CMAKE_PREFIX_PATH in reg env

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -46,9 +46,15 @@ set "VSINSTALLDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\"
 )
 
 IF NOT "%CONDA_BUILD%" == "" (
+  :: building packages
   set "INCLUDE=%LIBRARY_INC%;%INCLUDE%"
   set "LIB=%LIBRARY_LIB%;%LIB%"
   set "CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%;%CMAKE_PREFIX_PATH%"
+) else (
+  :: normal environment
+  set "INCLUDE=%CONDA_PREFIX%\Library\include;%INCLUDE%"
+  set "LIB=%CONDA_PREFIX%\Library\lib;%LIB%"
+  set "CMAKE_PREFIX_PATH=%CONDA_PREFIX%\Library;%CMAKE_PREFIX_PATH%"
 )
 
 
